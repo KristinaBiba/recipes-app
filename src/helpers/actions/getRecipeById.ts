5fffa32b9b6163bfbcbb48365b802fcbdd88a72e
Kristina <106387313+KristinaBiba@users.noreply.github.com>
2025-03-16 21:41:28 +0200

    Update getRecipeById.ts

--- src/helpers/actions/getRecipeById.ts
+++ src/helpers/actions/getRecipeById.ts
@@ -4,7 +4,7 @@ import {notFound} from 'next/navigation';
 export async function getRecipeById(id: string): Promise<MealInter> {
     const res: {meals: MealInter[] | string} = await fetch(`${process.env.NEXT_PUBLIC_API_LINK}/lookup.php?i=${id}`).then((r) => r.json());
 
-    if (typeof res === 'string') return notFound();
+    if (typeof res.meals === 'string') return notFound();
 
     return res.meals[0];
 }
