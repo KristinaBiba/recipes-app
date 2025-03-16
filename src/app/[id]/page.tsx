523c411e6f94eb0060cbf5ee9a0834cd824828f1
Kristina <106387313+KristinaBiba@users.noreply.github.com>
2025-03-16 21:23:13 +0200

    git settings

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,11 +1,18 @@
 import {Metadata} from 'next';
 import {getRecipeById} from '@/helpers/actions/getRecipeById';
 import {RecipePageView} from '@/views/RecipePageView';
+import {getAllRecipes} from '@/helpers/actions/getAllRecipes';
 
 interface RecipePageProps {
     params: Promise<{
         id: string;
     }>;
+}
+
+export async function generateStaticParams() {
+    const data = await getAllRecipes();
+
+    return data.map((item) => ({id: item.idMeal}));
 }
 
 export async function generateMetadata(props: RecipePageProps): Promise<Metadata> {
